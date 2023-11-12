8e4ee78e4a8dbe593737ecf6be5ad681baa0d15b
Leo Chan <leo.chan@tokuindustry.com>
2023-11-12 13:19:03 -0700

    Move tests into main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const testing = std.testing;
+
 export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
@@ -10,4 +13,20 @@ export fn hello() [*:0]const u8 {
 /// Return the given input string
 export fn echo(s: [*:0]const u8) [*:0]const u8 {
     return s;
+}
+
+test "basic add functionality" {
+    try testing.expect(add(3, 7) == 10);
+}
+
+test "hello" {
+    const h = hello();
+    try testing.expect(std.mem.len(h) == 5);
+    try testing.expect(std.mem.eql(u8, h[0..5], "hello"));
+}
+
+test "echo" {
+    const e = echo("goodbye");
+    try testing.expect(std.mem.len(e) == 7);
+    try testing.expect(std.mem.eql(u8, e[0..7], "goodbye"));
 }
